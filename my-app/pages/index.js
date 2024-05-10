27845f778c1ba962670a9bb3e183f48d73247d7c
sagarbhmr@gmail.com <sagarbhr@gmail.com>
2024-05-10 07:48:13 +0530

    json filedata added

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { useRef } from 'react';
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
@@ -6,7 +7,43 @@ import styles from "@/styles/Home.module.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const emailInputRef = useRef();
+const feedbackInputRef = useRef();
+
+function submitFormHandler(event){
+  event.preventDefault();
+  const enteredEmail = emailInputRef.current.value;
+  const enteredFeedback = feedbackInputRef.current.value;
+
+  const reqBody = { email: enteredEmail, text: enteredFeedback };
+
+  fetch('/api/feedback',{
+    method: 'POST', 
+    body: JSON.stringify(reqBody),
+    headers: {
+      'Content-Type':'application/json'
+    }
+  }).then( response => response.json())
+    .then( (data) => console.log(data));
+
+}
   return (
-    <h1>The Home Page</h1>
+    <div>
+      <h1>The Home Page</h1>
+    <form onSubmit={submitFormHandler}>
+        <div>
+          <label for='email'>Your Email Address</label>
+          <input type='email' id='email' ref={emailInputRef} />
+        </div>
+        <div>
+          <label for='feedback'>Your  feedback</label>
+          <textarea  id='feedback' rows="5" ref={feedbackInputRef} />
+        </div>
+        <button>
+            Send Feedback
+        </button>
+    </form> 
+    </div>
+    
   );
 }
